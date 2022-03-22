fa09a0680005f9fb077fa6377e0d90bfef3c7783
Amaury Vallet <amaury.vallet@gmail.com>
2022-03-22 18:20:43 +0100

    rajout des pundit & new create

--- app/controllers/apes_controller.rb
+++ app/controllers/apes_controller.rb
@@ -1,11 +1,61 @@
 class ApesController < ApplicationController
+    before_action :set_ape, only: [:show, :edit, :update, :destroy]
+    skip_before_action :authenticate_user!, only: [:show, :index]
+  
     def index
-        @apes = Ape.all
+      @apes = policy_scope(Ape)
     end
-
-
+  
     def show
-        @ape = Ape.find(params[:id])
-        @review = Review.new
+      authorize @ape
+      @review = Review.new
     end
+  
+    def new
+      @ape = Ape.new
+      authorize @ape
+    end
+  
+    def create
+      @ape = Ape.new(ape_params)
+      @ape.user = current_user
+      authorize @ape
+  
+      if @ape.save
+        redirect_to ape_path(@ape), notice: 'ape was successfully created.'
+      else
+        render :new
+      end
+    end
+  
+    def edit
+      authorize @ape
+    end
+  
+    def update
+      authorize @ape
+      if @ape.update(ape_params)
+          redirect_to ape_path(@ape), notice: 'Your Ape was successfully updated.'
+      else
+        render :edit
+      end
+    end
+  
+    def destroy
+      authorize @ape
+      @ape.destroy
+  
+      redirect_to apes_path, notice: 'Your Ape was successfully destroyed.'
+    end
+  
+    private
+  
+    def set_ape
+      @ape = Ape.find(params[:id])
+    end
+  
+    def ape_params
+      params.require(:ape).permit(:name, :address)
+    end   
+
 end
